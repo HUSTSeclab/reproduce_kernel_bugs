aea7b40e5dcd66300c55024ffc157d9d11dddcef
Dongliang Mu <mudongliangabcd@gmail.com>
2021-04-02 01:25:08 +0800

    add more kernel repos

--- Kernel_repos/clone_repos.sh
+++ Kernel_repos/clone_repos.sh
@@ -3,8 +3,8 @@
 git clone git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
 #git clone git://git.kernel.org/pub/scm/linux/kernel/git/netdev/net.git
 #git clone git://git.kernel.org/pub/scm/linux/kernel/git/netdev/net-next.git
-# linux-next
-# linux-next-history
-# bpf
-# bpf-next
-# kmsan
+#git clone https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git
+#git clone git://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next-history.git
+#git clone https://git.kernel.org/pub/scm/linux/kernel/git/bpf/bpf.git
+#git clone https://git.kernel.org/pub/scm/linux/kernel/git/bpf/bpf-next.git
+#git clone https://github.com/google/kmsan
